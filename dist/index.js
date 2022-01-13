3439187a555514d799929fe7deb862c3fd33d4c3
tyomhk <cheertrain15@naver.com>
2022-01-14 00:13:52 +0900

    TS and compiled JS

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, "2020202020202", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
+//# sourceMappingURL=index.js.map
