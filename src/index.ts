2038831836391237997d954e2b907f7ff6df338f
tyomhk <cheertrain15@naver.com>
2022-01-15 15:41:33 +0900

    Added oversimplified blockchain feature on TS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import * as CryptoJS from 'crypto-js'; // For encrypting data.
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static createBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean =>
+  typeof aBlock.index === "number" &&
+  typeof aBlock.hash === "string" &&
+  typeof aBlock.previousHash === "string" &&
+  typeof aBlock.timestamp === "number" &&
+  typeof aBlock.data === "string";
+
   constructor(
     index: number,
     hash: string,
@@ -19,8 +36,71 @@ class Block {
   }
 }
 
-const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, 'a78f7e9120283c', '', 'Hello', 1600000000);
 
-let blockchain: [Block] = [genesisBlock];
+// The first block on the chain.
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.createBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.createBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+createNewBlock("YAGOO");
+createNewBlock("Tokino Sora");
+createNewBlock("Haachama");
+createNewBlock("Oozora Subaru");
+createNewBlock("Ahoy");
+createNewBlock("52KG-Grip Angel");
+createNewBlock("Yukihana lamy");
+createNewBlock("La+");
 
 console.log(blockchain);
